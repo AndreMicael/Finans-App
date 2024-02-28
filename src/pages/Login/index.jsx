a75bb415ab12c0270185086bd70e9236423bd0f7
Andre Sampaio <andremicael@gmail.com>
2024-02-28 19:33:22 -0400

    Tela Login adicionada

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react";
 import "./Login.module.css";
 
 
 const Login = () => {
+  const [email,setEmail] = useState("");
+  const [password,setPassword] = useState("");
   return (
     <div>
-        <h1>Login</h1>
+        <h1>Fazer Login</h1>
+        <p>Preencha os dados corretamente para acessar a sua conta.</p>
+        <form>
+          <label>
+          <span>E-mail:</span>
+          <input 
+          type="email"
+          name='email'
+          required
+          placeholder='E-mail cadastrado'
+          value={email}
+          onChange={(e)=> setEmail(e.target.value)}/>
+          </label>
+
+          <label>
+          <span>Senha:</span>
+          <input 
+          type="password"
+          name='password'
+          required
+          placeholder='Insira sua senha'
+          value={password}
+          onChange={(e)=> setPassword(e.target.value)}/>
+          </label>
+        </form>
     </div>
   )
 }
