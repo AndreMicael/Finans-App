1f0672fcb5e69e46dc2d2f3bfcc46714fe4a4f23
Andre Sampaio <andremicael@gmail.com>
2024-02-28 21:27:12 -0400

    Página Cadastro Adicionada

--- src/pages/Cadastrar/index.jsx
+++ src/pages/Cadastrar/index.jsx
@@ -1,8 +1,72 @@
-import React from 'react'
+import styles from './Register.module.css';
+import { useState } from 'react';
 
 const Cadastrar = () => {
+
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error,setError] = useState('');
+
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Cadastrar</div>
+    <div className={styles.register}>
+        <h1>Cadastre-se</h1>
+      <p>Você está a um passo do controle financeiro</p>
+      <form className='container' onSubmit={handleSubmit}>
+        <label>
+          <span>Nome:</span>
+          <input 
+          type="text"
+          name='displayName'
+          required
+          placeholder='Nome do Usuário'
+          value={displayName}
+          onChange={(e)=> setDisplayName(e.target.value)}/>
+        </label>
+
+        <label>
+          <span>Email:</span>
+          <input 
+          type="email"
+          name='email'
+          required
+          placeholder='E-mail do Usuário'
+          value={email}
+          onChange={(e)=> setEmail(e.target.value)}/>
+        </label>
+
+        <label>
+          <span>Senha:</span>
+          <input 
+          type="password"
+          name='password'
+          required
+          placeholder='Insira sua senha'
+          value={password}
+          onChange={(e)=> setPassword(e.target.value)}/>
+        </label>
+        <label>
+          <span>Confirmar Senha:</span>
+          <input 
+          type="password"
+          name='repeatPassword'
+          required
+          placeholder='Confirme a sua senha '
+          value={confirmPassword}
+          onChange={(e)=> setConfirmPassword(e.target.value)}/>
+        </label>
+      <button className='btn'>Cadastrar</button>
+     
+    
+      </form>
+
+    </div>
   )
 }
 
